7c0c33df9fd633dff31b483da4a710620f2a5c5a
bailooon <matheus@bailon.com.br>
2025-04-07 22:12:28 -0300

    finalizando interface do desafio no terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,28 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner entrada = new Scanner(System.in);
         
+        System.out.println("Bem vindo ao seu banco!");
+        System.out.println("Digite o número da sua conta:");
+        numero = entrada.nextInt();
+        System.out.println("Digite o número da agência:");
+        agencia = entrada.next();
+        System.out.println("Digite o seu nome:");
+        nomeCliente = entrada.next();
+        System.out.println("Digite o saldo da sua conta:");
+        saldo = entrada.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque");
+
+        entrada.close();
     }
 }
